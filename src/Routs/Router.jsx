fd49257ca53198e09cde7f8989d2cee0eec35c5c
Sadek Ali <sadekali1530@gmail.com>
2023-11-12 23:58:57 +0600

    add

--- src/Routs/Router.jsx
+++ src/Routs/Router.jsx
@@ -1,11 +1,16 @@
 
 import { createBrowserRouter } from 'react-router-dom';
+import Home from '../Pages/Home/Home/Home';
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <div>Hello world!</div>,
       children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        }
         
       ]
     },
