8c5cdf0f7cad0dd3b9ee49faf43985e82945f24f
Sadek Ali <sadekali1530@gmail.com>
2023-11-13 00:16:25 +0600

    add

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
 
 
+
 const Home = () => {
     return (
         <div>
-            <h1>This is home </h1>
+           
         </div>
     );
 };
--- src/laout/Main.jsx
+++ src/laout/Main.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+
+import Navbar from '../Share/Navbar/Navbar';
+import Footer from '../Share/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
