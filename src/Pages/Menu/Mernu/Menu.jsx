928fe1d4c4b6a82e141f366978a2d3b4ca1a15e0
Sadek Ali <sadekali1530@gmail.com>
2023-11-13 23:10:05 +0600

    set helmeat title

--- src/Pages/Menu/Mernu/Menu.jsx
+++ src/Pages/Menu/Mernu/Menu.jsx
@@ -1,8 +1,14 @@
+import { Helmet } from "react-helmet-async";
 
 
 const Menu = () => {
     return (
         <div>
+
+            <Helmet>
+                <title>bistro boss |menu</title>
+                
+            </Helmet>
             <h1>Man mau manue</h1>
         </div>
     );
