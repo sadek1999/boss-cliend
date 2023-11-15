cae74333c8fac5351d97a7648aad6ec69a3e451f
Sadek Ali <sadekali1530@gmail.com>
2023-11-15 21:03:21 +0600

    add

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
                         </div>
                         <div className="form-control mt-6">
                             
-                            <input className="btn btn-primary" type="submit"  value="Login" />
+                            <input className="btn btn-primary" type="submit" onSubmit={handlsubmit} value="Login" />
                         </div>
                     </form>
                 </div>
