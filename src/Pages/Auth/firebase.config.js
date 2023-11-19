b11b07a7ce4d08648133045c5bc1414d5eb8dd74
Sadek Ali <sadekali1530@gmail.com>
2023-11-19 23:17:44 +0600

    add

---
+++ src/Pages/Auth/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrva9836vJ8Dkp0XqFCzwWt9PySq8KJ_g",
+  authDomain: "boss-1.firebaseapp.com",
+  projectId: "boss-1",
+  storageBucket: "boss-1.appspot.com",
+  messagingSenderId: "176982014419",
+  appId: "1:176982014419:web:eb6d8ff658cec9c5571726"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/Pages/Login/Singup.jsx
@@ -0,0 +1,11 @@
+
+
+const Singup = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Singup;
